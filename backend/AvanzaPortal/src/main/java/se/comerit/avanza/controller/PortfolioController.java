c6f35fa09e9532d340e161265243a71ab2e66f21
Mario <zalac.mario1@gmail.com>
2026-09-01 11:29:11 +0200

    import fix

--- backend/AvanzaPortal/src/main/java/se/comerit/avanza/controller/PortfolioController.java
+++ backend/AvanzaPortal/src/main/java/se/comerit/avanza/controller/PortfolioController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import jakarta.servlet.http.HttpSession;
-import se.comerit.avanza.service.DashboardService;
 import se.comerit.avanza.service.PortfolioService;
 
 import java.util.ArrayList;
