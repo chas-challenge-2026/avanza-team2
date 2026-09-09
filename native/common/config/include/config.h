2b6803672d5edbd3e91239a675d24da1d49fe2c2
tjackr <tjackr@proton.me>
2026-09-09 17:28:36 +0200

    Added common config.h for setting dir paths etc

---
+++ native/common/config/include/config.h
@@ -0,0 +1,12 @@
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+#define LOG_ROOT_DIR "/var/log/avanza2/"
+
+#define DATA_ROOT_DIR "/var/lib/avanza2/"
+#define DATA_CACHE_DIR "/var/lib/avanza2/cache/"
+#define DATA_CACHE_RATE_DIR "/var/lib/avanza2/cache/rate/"
+
+// TODO: Function that ensures dirs are created and permissiible to write to
+
+#endif
