40b5a9cc17b756101e09c3837a305f3d1d77ebb6
Mario <zalac.mario1@gmail.com>
2026-09-09 11:43:53 +0200

    Deleting unsed and duplicated dto-s

--- backend/AvanzaPortal/src/main/java/se/comerit/avanza/dto/alerts/AlertsResponseDTO.java
+++ backend/AvanzaPortal/src/main/java/se/comerit/avanza/dto/alerts/AlertsResponseDTO.java
@@ -1,9 +1,9 @@
 package se.comerit.avanza.dto.alerts;
 
 public record AlertsResponseDTO(
-                Long id,
-                Long user,
-                String message,
-                boolean dismissed,
-                String createdAt) {
+        Long id,
+        Long user,
+        String message,
+        boolean dismissed,
+        String createdAt) {
 }
